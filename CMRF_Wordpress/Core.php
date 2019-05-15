ec7c0daa43ef6e753b421136ba6044505fd80b65
B. Endres <endres@systopia.de>
2019-05-15 21:37:37 +0200

    cleaning up

--- CMRF_Wordpress/Core.php
+++ CMRF_Wordpress/Core.php
@@ -1,42 +1,21 @@
 <?php
 
 /**
- * Drupal-based implementation of a CMRF Core
+ * Wordpress-based implementation of a CMRF Core
  *
- * @author Björn Endres, SYSTOPIA (endres@systopia.de)
+ * @author TODO
  */
 
-namespace CMRF\Drupal;
+namespace CMRF\Wordpress;
 
 require_once(dirname(__FILE__) .'/Call.php');
 require_once(dirname(__FILE__) .'/Connection/Curl.php');
-require_once(dirname(__FILE__) .'/SQLPersistingCallFactory.php');
 
 use CMRF\Core\Core         as AbstractCore;
 use CMRF\Core\Connection;
-use CMRF\Drupal\SQLPersistingCallFactory;
-
 
 class Core extends AbstractCore {
-
-  protected $connections = array();
-
-  public function __construct() {
-    $info = \Database::getConnectionInfo('default');
-    $info = $info['default'];
-    if(!isset($info['port']) || empty($info['port'])) {
-      $info['port'] = NULL;
-    }
-
-    $table_name = \Database::getConnection()->prefixTables("{cmrf_core_call}");
-    $connection = new \mysqli($info['host'],$info['username'],$info['password'],$info['database'],$info['port']);
-    $factory = new SQLPersistingCallFactory($connection, $table_name, array('\CMRF\Drupal\Call','createNew'), array('\CMRF\Drupal\Call','createWithRecord'));
-    parent::__construct($factory);
-  }
-
-  public function getDefaultProfile() {
-    $profile = cmrf_core_default_profile();
-    return $profile['name'];
+    parent::__construct(null);
   }
 
   /**
@@ -47,19 +26,10 @@ class Core extends AbstractCore {
    * @return Connection
    */
   protected function getConnection($connector_id) {
-    if (!isset($this->connections[$connector_id])) {
-      $connectors = cmrf_core_list_connectors();
-      $profile = $this->getConnectionProfile($connector_id);
-      if (!isset($connectors[$profile['connector']])) {
-        watchdog('cmrf_core', t('No connector available for %connector_name', array('connector_name' => $profile['connector'])), array(), WATCHDOG_ERROR);
-      }
-      if (!isset($connectors[$profile['connector']]['callback']) || !function_exists($connectors[$profile['connector']]['callback'] )) {
-        watchdog('cmrf_core', t('No connector available for %connector_name', array('connector_name' => $profile['connector'])), array(), WATCHDOG_ERROR);
-      }
-      $this->connections[$connector_id] = call_user_func($connectors[$profile['connector']]['callback'], $this, $connector_id);
-    }
-
-    return $this->connections[$connector_id];
+    // TODO: get core;
+    $core = null; 
+    // TODO: cache
+    return new Connection($core, $connector_id);
   }
 
 
@@ -67,24 +37,35 @@ class Core extends AbstractCore {
    *  Use Drupal variables to store config for the moment  *
    *********************************************************/
 
+  public function getDefaultProfile() {
+    // TODO
+    //$profile = cmrf_core_default_profile();
+    //return $profile['name'];
+  }
+
   public function getConnectionProfiles() {
-    return cmrf_core_list_profiles();
+    // TODO: 
+    // return cmrf_core_list_profiles();
   }
 
   public function getRegisteredConnectors() {
-    return variable_get('cmrf_core_connectors');
+    // TODO
+    // return variable_get('cmrf_core_connectors');
   }
 
   protected function storeRegisteredConnectors($connectors) {
-    return variable_set('cmrf_core_connectors', $connectors);
+    // TODO
+    // return variable_set('cmrf_core_connectors', $connectors);
   }
 
   public function getSettings() {
-    return variable_get('cmrf_core_settings');
+    // TODO
+    // return variable_get('cmrf_core_settings');
   }
 
   protected function storeSettings($settings) {
-    return variable_set('cmrf_core_settings', $settings);
+    // TODO
+    // return variable_set('cmrf_core_settings', $settings);
   }
 
 }
